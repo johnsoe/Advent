e825c069ab5eacd03b22db727612554b4448105b
johnsoe <evan.johnson.r@gmail.com>
2019-12-14 14:09:49 -0800

    Day Thirteen Part 2

--- main/src/main/java/advent/tasks/DayThirteen.kt
+++ main/src/main/java/advent/tasks/DayThirteen.kt
@@ -24,6 +24,50 @@ class DayThirteen: Day() {
         }
     }
 
+    fun playBrickBreaker(): Int {
+        val input = getInputBySeparator().map { it.toLong() }.toMutableList()
+        input[0] = 2L
+        val comp = LongCodeComputer(input)
+
+        var state = GameState()
+        var opCount = 0
+        while (comp.lastOpcode != LongCodeComputer.Opcode.Terminate) {
+            val outputs = comp.parseAllInstructions(listOf(state.joystick.toLong()), 3).map { it.toInt() }
+            if (outputs.size != 3) break
+            if (outputs[0] == -1 && outputs[1] == 0) {
+                state.score = outputs[2]
+            } else {
+                val tile = getTile(outputs[2])
+                val point = Point(outputs[0], outputs[1])
+                state.board[point] = tile
+
+                when (tile) {
+                    Tile.Paddle -> state.paddlePosition = point
+                    Tile.Ball -> state.ballPosition = point
+                }
+                updateJoystick(state)
+            }
+            opCount++
+        }
+        return state.score
+    }
+
+    private fun updateJoystick(state: GameState) {
+        when {
+            state.ballPosition.x < state.paddlePosition.x -> state.joystick = -1
+            state.ballPosition.x > state.paddlePosition.x -> state.joystick = 1
+            else -> state.joystick = 0
+        }
+    }
+
+    private class GameState {
+        var score = 0
+        var paddlePosition = Point(0, 0)
+        var ballPosition = Point(0, 0)
+        var joystick = 0
+        var board = mutableMapOf<Point, Tile>()
+    }
+
     private fun getTile(id: Int): Tile {
         return when(id) {
             0 -> Tile.Empty
@@ -36,10 +80,10 @@ class DayThirteen: Day() {
     }
 
     sealed class Tile {
-        object Empty: Tile()
-        object Wall: Tile()
-        object Block: Tile()
-        object Paddle: Tile()
-        object Ball: Tile()
+        object Empty : Tile()
+        object Wall : Tile()
+        object Block : Tile()
+        object Paddle : Tile()
+        object Ball : Tile()
     }
 }
